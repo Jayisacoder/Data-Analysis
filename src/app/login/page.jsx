4b4fbc6643814e7463f241f87aedd3fbbbf02231
JayFromHyrule <jgree0075@launchpadphilly.org>
2025-11-24 20:59:07 -0500

    Delete login page to fix Next.js build error

--- src/app/login/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function LoginPage() {
-	return (
-		<div>
-			<h1>Login Page</h1>
-			{/* Add your login form or content here */}
-		</div>
-	);
-}
-// This page has been removed. No login functionality.
